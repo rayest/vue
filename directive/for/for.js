978ead14b02d305520161fac1014a2fbea3eba50
lirui <634178463@qq.com>
2017-05-20 23:44:38 +0800

    Add for directive

---
+++ directive/for/for.js
@@ -0,0 +1,25 @@
+/**
+ * Created by lirui on 2017/5/20.
+ */
+
+new Vue({
+    el: '#app',
+    data: {
+        people: [
+            {
+                name: 'Rayest',
+                age: 26,
+                sex: 'male'
+            },
+            {
+                name: 'Pay',
+                age: 22,
+                sex: 'female'
+            },
+            {
+                name: 'lee',
+                age: 26,
+                sex: 'male'
+            }]
+    }
+});
