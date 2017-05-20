5d3b650c7e37fc079662e0d31e9c9c2583d51634
lirui <634178463@qq.com>
2017-05-20 22:05:14 +0800

    Add description of view、model and viewModel

--- hello/hello.js
+++ hello/hello.js
@@ -2,12 +2,12 @@
  * Created by lirui on 2017/5/20.
  */
 
-// model
+// 定义 model
 var exampleData = {
     message: 'Hello World!'
 };
 
-// 创建一个 Vue 实例或 "ViewModel", 它连接 View 与 Model
+// 创建一个 Vue 实例或 "ViewModel", 连接 View 与 Model
 new Vue({
     el: '#app',
     data: exampleData
