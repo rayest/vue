fa3dcfe4d7e256fedbba5afdbf92bd8ea06dd69d
lirui <634178463@qq.com>
2017-05-20 21:55:46 +0800

    Creation of hello demo

---
+++ hello/hello.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lirui on 2017/5/20.
+ */
+
+// model
+var exampleData = {
+    message: 'Hello World!'
+};
+
+// 创建一个 Vue 实例或 "ViewModel", 它连接 View 与 Model
+new Vue({
+    el: '#app',
+    data: exampleData
+});
